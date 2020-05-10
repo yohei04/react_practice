e6733fe81a6cfe4d2def011187df9dee6f121111
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-10 14:02:38 +0900

    basic stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { userState, useState } from 'react';
+import Tweet from './Tweet'
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: 'ED', message: 'Hello there' },
+    { name: 'Tadashi', message: "I don't know" },
+    { name: 'YesMan', message: 'Yes I will' },
+  ])
+
+
   return (
-    <div className="App">
+    <div>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
+
+
+      {/* <h1 className={isRed ? 'red' : ''}>color</h1>
+      <button onClick={increment}>increment</button>
+      <h1>{count}</h1> */}
     </div>
   );
 }
