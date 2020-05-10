e00507f4299b30dcee5848001bad97c070fd3567
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-10 15:19:38 +0900

    make nav

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div style={nav}>
+      <h3>Dev Ed</h3>
+      <p>List of Movies:</p>
+    </div>
+  )
+}
+
+
+const nav = {
+  background: 'black',
+  color: 'white',
+}
+
+export default Nav
+
