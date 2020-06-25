f4e742f18aa89526f726fbf89ab59820e219a240
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-25 13:10:15 +0900

    Tutorial6-1 my answer

--- src/hypertextcandy/Tutorial61.js
+++ src/hypertextcandy/Tutorial61.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Tutorial61 = () => {
-  
+  const [users, setUsers] = useState(null);
+
+  const url =
+    'https://randomuser.me/api/?results=5&nat=us&inc=gender,name,email';
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch(url).then((res) => res.json());
+      setUsers(response.results);
+    };
+    fetchUser();
+  }, []);
+
+  console.log(users);
 
   return (
     <table>
-      <tr>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Email</th>
-      </tr>
-      <tr>
-        <td>test1</td>
-        <td>test2</td>
-        <td>test3</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Gender</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users
+          ? users.map((user) => (
+              <tr>
+                <td>
+                  {user.name.title} {user.name.first} {user.name.last}
+                </td>
+                <td>{user.gender}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))
+          : ''}
+      </tbody>
     </table>
   );
-}
+};
 
-export default Tutorial61
-
+export default Tutorial61;
